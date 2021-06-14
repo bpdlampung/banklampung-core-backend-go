19d2493b2401698d425fc60eb907618d2426aeab
Ichwan Almaza <mail@ichwan.me>
2021-06-14 10:42:05 +0700

    time to string

--- helpers/date.go
+++ helpers/date.go
@@ -10,8 +10,16 @@ type DateTime struct {
 	time.Time
 }
 
-const dateTimeFormat = "02-01-2006 15:04:05"
-const dateFormat = "02-01-2006"
+const DateTimeFormat = "02-01-2006 15:04:05"
+const DateFormat = "02-01-2006"
+
+// time database to Asia/Jakarta
+func DateTimeToString(dateTime time.Time) string {
+	locName := GetEnvAndValidate("TZ")
+	loc, _ := time.LoadLocation(locName)
+
+	return dateTime.In(loc).Format(DateTimeFormat)
+}
 
 func (ct *DateTime) UnmarshalJSON(b []byte) (err error) {
 	s := strings.Trim(string(b), "\"")
@@ -20,7 +28,7 @@ func (ct *DateTime) UnmarshalJSON(b []byte) (err error) {
 		return
 	}
 	loc, _ := time.LoadLocation("Asia/Jakarta")
-	ct.Time, err = time.ParseInLocation(dateTimeFormat, s, loc)
+	ct.Time, err = time.ParseInLocation(DateTimeFormat, s, loc)
 	return
 }
 
@@ -28,7 +36,7 @@ func (ct *DateTime) MarshalJSON() ([]byte, error) {
 	if ct.Time.UnixNano() == nilTime {
 		return []byte("null"), nil
 	}
-	return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format(dateTimeFormat))), nil
+	return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format(DateTimeFormat))), nil
 }
 
 var nilTime = (time.Time{}).UnixNano()
@@ -46,7 +54,7 @@ func (ct *Date) UnmarshalJSON(b []byte) (err error) {
 		return
 	}
 	loc, _ := time.LoadLocation("Asia/Jakarta")
-	ct.Time, err = time.ParseInLocation(dateTimeFormat, s, loc)
+	ct.Time, err = time.ParseInLocation(DateTimeFormat, s, loc)
 	return
 }
 
@@ -54,5 +62,5 @@ func (ct *Date) MarshalJSON() ([]byte, error) {
 	if ct.Time.UnixNano() == nilTime {
 		return []byte("null"), nil
 	}
-	return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format(dateTimeFormat))), nil
+	return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format(DateTimeFormat))), nil
 }
