28e7956ec9a05c7c3a3382795fb477e1c3e61106
Ichwan Almaza <mail@ichwan.me>
2021-07-31 00:09:58 +0700

    sacn redis

--- databases/redis/interface.go
+++ databases/redis/interface.go
@@ -12,6 +12,7 @@ type Collections interface {
 	Set(key string, value interface{}, expired time.Duration) error
 	Get(key string, value interface{}) error
 	Del(key string)
+	Scan(prefix string) ([]string, error)
 }
 
 func (r Redis) Set(key string, value interface{}, expired time.Duration) error {
@@ -39,4 +40,24 @@ func (r Redis) Get(key string, value interface{}) error {
 
 func (r Redis) Del(key string) {
 	r.client.Del(context.Background(), key)
+}
+
+func (r Redis) Scan(prefix string) ([]string, error) {
+	var keys []string
+
+	iterator := r.client.Scan(context.Background(), 0, prefix, 0).Iterator()
+
+	for iterator.Next(context.Background()) {
+		keys = append(keys, iterator.Val())
+	}
+
+	if err := iterator.Err(); err != nil {
+		return nil, err
+	}
+
+	if keys == nil || len(keys) == 0 {
+		return nil, nil
+	}
+
+	return keys, nil
 }
