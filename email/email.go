464c0324f2b43d05967b78dae7bac5bcde2eb9e8
Ichwan Almaza <mail@ichwan.me>
2023-01-31 10:56:57 +0700

    add attach email

--- email/email.go
+++ email/email.go
@@ -8,6 +8,10 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
+const (
+	EmailAttachmentDirectory = "./configs/resources/email-attachment/"
+)
+
 type EmailConfig struct {
 	emailDialler *gomail.Dialer
 	logger       logs.Collections
@@ -23,6 +27,7 @@ type SendMail struct {
 	To      []string
 	Cc      []string
 	Bcc     []string
+	Attach  []string
 }
 
 func InitConfig(mailPort int, mailHost, mailSender, username, password, mailTrap string, tlsStatus bool, logger logs.Collections, env enums.Environment) EmailConfig {
@@ -54,6 +59,11 @@ func (cfg EmailConfig) SendMail(payload SendMail) error {
 	mailer.SetHeader("Bcc", payload.Bcc...)
 	mailer.SetHeader("Subject", payload.Subject)
 	mailer.SetBody("text/html", payload.Content)
+	if payload.Attach != nil || len(payload.Attach) > 0 {
+		for _, val := range payload.Attach {
+			mailer.Attach(val)
+		}
+	}
 
 	err := cfg.emailDialler.DialAndSend(mailer)
 
